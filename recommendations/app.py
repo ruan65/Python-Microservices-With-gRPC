da101e2928752bdcfc75b975a092e1420576c141
Andrey Rudenko <arudenko@corp.finam.ru>
2021-03-14 23:29:48 +0300

    initial

---
+++ recommendations/app.py
@@ -0,0 +1,9 @@
+from reccomendations_pb2 import BookCategory, RecommendationsRequest
+import grpc
+from reccomendations_pb2_grpc import RecommendationsStub
+
+channel = grpc.insecure_channel("localhost:50051")
+client = RecommendationsStub(channel)
+
+request = RecommendationsRequest(user_id=1, category=BookCategory.SELF_HELP, max_results=2)
+print(client.Recommend(request))
