e1a0975629fe57c8c4b43f1a3de03337b706608e
Andreyka <ruan65@gmail.com>
2021-03-21 14:11:34 +0300

    all is up

--- scripts.sh
+++ scripts.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 # docker build . -f recommendations/Dockerfile -t recommendations
-# docker run -p 127.0.0.1:50051:50051/tcp recommendations
+# docker network create microservices
+# docker run -p 127.0.0.1:50051:50051/tcp --network microservices --name req recommendations
 # docker container start admiring_satoshi
 # FLASK_APP=marketplace.py flask run
+
+# docker run -p 127.0.0.1:5000:5000/tcp --network microservices -e RECOMMENDATIONS_HOST=rec marketplace
